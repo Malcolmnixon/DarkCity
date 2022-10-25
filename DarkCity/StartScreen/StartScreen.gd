150b4ed67b53ef7f14216986d3484ad91c18cfef
Malcolm Nixon <Malcolm.nixon@gmail.com>
2022-10-24 21:09:53 -0400

    Updated to Godot-xr-tools 3.0.0 Fixed best-time formatting for seconds.

--- DarkCity/StartScreen/StartScreen.gd
+++ DarkCity/StartScreen/StartScreen.gd
@@ -5,12 +5,12 @@ func _ready():
 	if GameState.last_time <= 0.0:
 		$LastTimeValue.text = "-:--.-"
 	else:
-		$LastTimeValue.text = "%d:%2.1f" % [ GameState.last_time / 60.0, fmod(GameState.last_time, 60.0) ]
+		$LastTimeValue.text = "%d:%02.1f" % [ GameState.last_time / 60.0, fmod(GameState.last_time, 60.0) ]
 
 	if GameState.best_time <= 0.0:
 		$BestTimeValue.text = "-:--.-"
 	else:
-		$BestTimeValue.text = "%d:%2.1f" % [ GameState.best_time / 60.0, fmod(GameState.best_time, 60.0) ]
+		$BestTimeValue.text = "%d:%02.1f" % [ GameState.best_time / 60.0, fmod(GameState.best_time, 60.0) ]
 
 func _on_StartButton_pressed():
 	GameSignals.emit_signal("game_started")
